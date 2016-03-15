7f413887469916c9c0b5f35f917d1950f7511ab6
celogio <caseyelogio@Caseys-MacBook-Pro.local>
2016-03-15 10:27:26 -0400

    added footer stuff

---
+++ js/footer.js
@@ -0,0 +1,11 @@
+$.get("http://celogio24.github.io/partials/footer.html", function(data){
+
+  $(document).ready(function(){
+         // $(".container"){display:none;}
+
+      $(".container").append(data);
+
+
+      })
+
+    })
