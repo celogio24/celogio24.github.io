d1ebbe510af7975df21533a26aab1e82f14ed328
celogio <caseyelogio@Caseys-MacBook-Pro.local>
2016-03-16 18:34:10 -0400

    tried again

--- otherProjects/ajaxExamples/js/client.js
+++ otherProjects/ajaxExamples/js/client.js
@@ -1,22 +1,22 @@
-alert("working");
+$(document).ready(function() {
+  $("#getClients").on("click", function() {
+
+    var url = "http://JennaPewPew.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json"
+    $.getJSON(url, function(data) {
+      var html = "<table class='table table-hover table-striped table-responsive'>" +
+        "<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
 
 
-$("#getClients").click(function() {
-  //will not work locally with Chrome (use Safari, FF)
-  $.getJSON("http://celogio24/otherProjects/ajaxExamples/jsonDatabase/clients.json", function(data) {
-    var table = "<table class='table table-striped table-hover'>" +
-      "<tr><th>Name</th><th>Company</th><th>Email</th><th>Phone</th><th>Balance</th></tr>";
-    $.each(data, function(index, item) {
-      table += "<tr>" +
+      $.each(data, function(index, item) {
+        html += "<tr>" +
         "<td>" + item.name + "</td>" +
-        "<td>" + item.company + "</td>" +
         "<td>" + item.email + "</td>" +
-        "<td>" + item.phone + "</td>" +
-        "<td>" + item.balance + "</td>" +
+        "<td>" + item.company + "</td>"+
         "</tr>";
-    });
-    table += "</table>";
-    $("#data").append(table);
-  }); //getJSON
-}); //click
-//to look at a JSON file clearly copy the contents of clients.json into http://www.jsoneditoronline.org/
+      })
+
+      html + "</table>";
+      $("#data").append(html);
+    })
+  })
+})
