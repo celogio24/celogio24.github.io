b5f76026cad9a2df990dafc2d5b5c4f835055e02
celogio <caseyelogio@Caseys-MacBook-Pro.local>
2016-03-31 21:41:45 -0400

    ???

--- finalProject/js/nav.js
+++ finalProject/js/nav.js
@@ -1,13 +1,14 @@
-$.get("partials/nav.html", function(data){
 
-    $(document).ready(function(){
-       $(".container-fluid").hide();
-    $(".container-fluid").prepend(data);
-        $(".container-fluid").show();
+$.get("http://celogio24.github.io/finalProject/partials/nav.html", function(data){
 
+  $(document).ready(function(){
+         // $(".container"){display:none;}
+          $(".container").hide();
+      $(".container").prepend(data);
 
+     // $("body").prepend(data);
+      //.container.fadeIn();
+          $(".container").fadeIn(1200);
+      })
 
     })
-
-
-})
